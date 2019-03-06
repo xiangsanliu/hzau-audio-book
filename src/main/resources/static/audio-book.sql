f8364672dece0ef20d610d579a8daaca951a2f01
xiangsanliu <xiangjianjian@hotmail.com>
2019-03-06 12:28:06 +0800

    更新数据库sql

--- src/main/resources/static/audio-book.sql
+++ src/main/resources/static/audio-book.sql
@@ -1,2 +1,34 @@
-create database audio_book default charset utf8 collate utf8_general_ci;
-use audio_book;
+create schema audio_book collate utf8_general_ci;
+
+create table book
+(
+  id      int auto_increment
+    primary key,
+  name    varchar(20)  null,
+  author  varchar(20)  null,
+  picPath varchar(100) null,
+  `desc`  varchar(100) null
+);
+
+create table book_audio
+(
+  id     int auto_increment
+    primary key,
+  path   varchar(100) null,
+  bookId int          null
+);
+
+create table book_list
+(
+  id   int auto_increment
+    primary key,
+  name varchar(20) null
+);
+
+create table book_list_book
+(
+  bookListId int null,
+  bookId     int null
+)
+  comment '书和书单关系';
+
