a4bce9376c41996f57876f02c07788ee63fc8cb2
xiangsanliu <xiangjianjian@hotmail.com>
2019-03-29 21:56:13 +0800

    允许跨域请求

---
+++ src/main/java/com/hzau/feidian/hzauaudiobook/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.hzau.feidian.hzauaudiobook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author 项三六
+ * @time 2019/3/29 21:31
+ * @comment
+ */
+
+@Configuration
+public class CorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+}
