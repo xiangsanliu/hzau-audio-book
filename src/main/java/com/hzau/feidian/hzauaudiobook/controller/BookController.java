ffb183d1581d2daba08676a07fc1c1c96412c50f
xiangsanliu <xiangjianjian@hotmail.com>
2019-04-15 21:08:54 +0800

    书籍海报上传

--- src/main/java/com/hzau/feidian/hzauaudiobook/controller/BookController.java
+++ src/main/java/com/hzau/feidian/hzauaudiobook/controller/BookController.java
@@ -1,12 +1,18 @@
 package com.hzau.feidian.hzauaudiobook.controller;
 
 import com.hzau.feidian.hzauaudiobook.service.BookService;
+import com.hzau.feidian.hzauaudiobook.service.FileService;
+import com.hzau.feidian.hzauaudiobook.share.Pair;
 import com.hzau.feidian.hzauaudiobook.share.ResponseBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("book/")
@@ -14,9 +20,12 @@ public class BookController {
 
     private final BookService bookService;
 
+    private final FileService fileService;
+
     @Autowired
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, FileService fileService) {
         this.bookService = bookService;
+        this.fileService = fileService;
     }
 
     @RequestMapping("getAllBooks")
@@ -34,6 +43,23 @@ public class BookController {
     public ResponseBean remove(@PathVariable("id") long id) {
         bookService.removeOne(id);
         return ResponseBean.ok("删除成功");
+    }
+
+    @RequestMapping("upload/{id}/{name}")
+    public ResponseBean uploadPic(MultipartFile file, @PathVariable("id") Long actId,
+                                  @PathVariable("name") String bookName) {
+        String parentDir = "books" + File.separator + bookName;
+        String fileName = bookName + ".jpg";
+        Pair<Boolean, String> result;
+        try {
+            result = fileService.uploadFile(file, parentDir, fileName, true);
+        } catch (IOException e) {
+            return ResponseBean.error("IO错误");
+        }
+        if (result.getKey()) {
+            return ResponseBean.ok("books/" + bookName + '/' + fileName);
+        }
+        return ResponseBean.error(result.getValue());
     }
 
 }
